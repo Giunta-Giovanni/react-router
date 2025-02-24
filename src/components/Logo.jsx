d26109729742ce3a5dd1a28ebda6cf647f0b0a0c
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-24 16:22:28 +0100

    [feat] home link in home

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
 export default function Logo() {
     return (
         <div className="box-logo">
-            <img src="./public/route.svg" alt="route-logo" />
+            <Link to={"/"}>
+                <img src="./public/route.svg" alt="route-logo" />
+            </Link>
+
         </div>
     )
 }
